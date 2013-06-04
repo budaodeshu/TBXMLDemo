eb435807ff6d854aae6fdf604ab90427ac0e695b
zheng <zhengliangshu@163.com>
2013-06-04 11:02:28 +0800

    Initial Commit

---
+++ TBXMLDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TBXMLDemo
+//
+//  Created by zheng on 13-6-4.
+//  Copyright (c) 2013年 liangshu.zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TBXMLDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TBXMLDemo
+//
+//  Created by zheng on 13-6-4.
+//  Copyright (c) 2013年 liangshu.zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
